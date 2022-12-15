2607252518eed4221ab5496019e88dad1177d779
user <yigityargi26@gmail.com>
2022-12-15 19:52:23 +0300

    .

---
+++ YemekSiparis/Properties/USER.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class USER
+    {
+    
+    public int ID {get; set;}
+        public string USERNAME { get; set; }
+        
+        public string PASSWORD  { get; set; }
+        public string EMAIL { get; set; }
+        public int  AGE{ get; set; }
+
+
+
+
+    }
+}
